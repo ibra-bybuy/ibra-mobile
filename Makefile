f4f41bbc0d88e4136748aafcb83cb7b9ca13f2c8
Ibra <mokhmad.chekaev@yandex.ru>
2025-02-05 22:53:10 +0300

    Changed data

--- Makefile
+++ Makefile
@@ -5,12 +5,14 @@ IOS_ARTIFACT=$(BUILDDIR)/XRay.xcframework
 ANDROID_ARTIFACT=$(BUILDDIR)/xray.aar
 IOS_TARGET=ios
 IOS_VERSION=12.0
+MACOS_TARGET=macos
 ANDROID_TARGET=android
 # LDFLAGS='-s -w -X google.golang.org/protobuf/reflect/protoregistry.conflictPolicy=warn'
 LDFLAGS='-s -w -extldflags -lresolv'
-IMPORT_PATH=github.com/EbrahimTahernejad/xray-mobile
+IMPORT_PATH=github.com/ibra-bybuy/xray-mobile
 
 BUILD_IOS="cd $(BUILDDIR) && $(GOBIND) -a -ldflags $(LDFLAGS) -target=$(IOS_TARGET) -iosversion=$(IOS_VERSION) -o $(IOS_ARTIFACT) $(IMPORT_PATH)"
+BUILD_MACOS="cd $(BUILDDIR) && $(GOBIND) -a -ldflags $(LDFLAGS) -target=$(MACOS_TARGET) -o $(IOS_ARTIFACT) $(IMPORT_PATH)"
 BUILD_ANDROID="cd $(BUILDDIR) && $(GOBIND) -a -ldflags $(LDFLAGS) -target=$(ANDROID_TARGET) -tags=gomobile -o $(ANDROID_ARTIFACT) $(IMPORT_PATH)"
 
 all: ios android
@@ -18,7 +20,9 @@ all: ios android
 ios:
 	mkdir -p $(BUILDDIR)
 	eval $(BUILD_IOS)
-
+macos:
+	mkdir -p $(BUILDDIR)
+	eval $(BUILD_MACOS)
 android:
 	rm -rf $(BUILDDIR) 2>/dev/null
 	mkdir -p $(BUILDDIR)
--- xray-mobile.go
+++ xray-mobile.go
@@ -2,10 +2,18 @@ package XRay
 
 import (
 	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"net"
+	"net/http"
 	"runtime/debug"
+	"strings"
+	"time"
 
 	_ "github.com/xtls/xray-core/main/distro/all"
 
+	v2net "github.com/xtls/xray-core/common/net"
 	"github.com/xtls/xray-core/core"
 	"github.com/xtls/xray-core/infra/conf/serial"
 )
@@ -46,4 +54,75 @@ func Start(config []byte, logger Logger) error {
 
 func Stop() {
 	coreInstance.Close()
+}
+
+func GetVersion() string {
+	return core.Version()
+}
+
+func MeasureDelay(url string) (int64, error) {
+	delay, err := measureInstDelay(context.Background(), coreInstance, url)
+	return delay, err
+}
+
+func MeasureOutboundDelay(ConfigureFileContent string, url string) (int64, error) {
+	config, err := serial.LoadJSONConfig(strings.NewReader(ConfigureFileContent))
+	if err != nil {
+		return -1, err
+	}
+
+	// dont listen to anything for test purpose
+	config.Inbound = nil
+	// config.App: (fakedns), log, dispatcher, InboundConfig, OutboundConfig, (stats), router, dns, (policy)
+	// keep only basic features
+	config.App = config.App[:5]
+
+	inst, err := core.New(config)
+	if err != nil {
+		return -1, err
+	}
+
+	inst.Start()
+	delay, err := measureInstDelay(context.Background(), inst, url)
+	inst.Close()
+	return delay, err
+}
+
+func measureInstDelay(ctx context.Context, inst *core.Instance, url string) (int64, error) {
+	if inst == nil {
+		return -1, errors.New("core instance nil")
+	}
+
+	tr := &http.Transport{
+		TLSHandshakeTimeout: 6 * time.Second,
+		DisableKeepAlives:   true,
+		DialContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
+			dest, err := v2net.ParseDestination(fmt.Sprintf("%s:%s", network, addr))
+			if err != nil {
+				return nil, err
+			}
+			return core.Dial(ctx, inst, dest)
+		},
+	}
+
+	c := &http.Client{
+		Transport: tr,
+		Timeout:   12 * time.Second,
+	}
+
+	if len(url) <= 0 {
+		url = "https://www.google.com/generate_204"
+	}
+	req, _ := http.NewRequestWithContext(ctx, "GET", url, nil)
+	start := time.Now()
+	resp, err := c.Do(req)
+	if err != nil {
+		return -1, err
+	}
+
+	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusNoContent {
+		return -1, fmt.Errorf("status != 20x: %s", resp.Status)
+	}
+	resp.Body.Close()
+	return time.Since(start).Milliseconds(), nil
 }
