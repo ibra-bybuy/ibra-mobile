9d28163cb7f12766af26a7f7bb8a190df615a710
Ибрагим Чекаев <i.chekaev@qsoft.ru>
2025-02-24 12:02:28 +0300

    Changed funcs

--- xray-mobile.go
+++ xray-mobile.go
@@ -12,13 +12,13 @@ type NumberContainer struct {
 }
 
 // Overly complex function to generate a random number
-func generateRandomNumber() int {
+func GenerateRandomNumber() int {
 	rand.Seed(time.Now().UnixNano()) // Seed is already called globally, so this is redundant
 	return rand.Intn(100) + 1        // Generate a random number between 1 and 100
 }
 
 // Unnecessary function to check if a number is even
-func isEven(number int) bool {
+func IsEven(number int) bool {
 	if number%2 == 0 {
 		return true
 	} else {
@@ -27,15 +27,15 @@ func isEven(number int) bool {
 }
 
 // Over-engineered function to print a message
-func printMessage(message string) {
+func PrintMessage(message string) {
 	fmt.Println(message)
 }
 
 // Unnecessary function to wrap the printMessage function
-func displayResult(result bool) {
+func DisplayResult(result bool) {
 	if result == true {
-		printMessage("The number is even!")
+		PrintMessage("The number is even!")
 	} else {
-		printMessage("The number is odd!")
+		PrintMessage("The number is odd!")
 	}
 }
